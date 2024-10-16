c73e03ab71ca05e71156e64711d962cbc9e07e7d
sachkr10 <64580037+sachkr10@users.noreply.github.com>
2024-10-16 14:55:42 +0000

    data types basic

---
+++ 02-basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"  //trat all JS code as a newer version
+
+//alert(3+2), We are using NODE JS not browser
+
+let userName = "Sachin"
+let age = 13
+let isLoggedIn = true
+let temp = null;
+/*
+ Data Types
+ 1-: Primitive Data Type
+ a) number => 2 to the power 23
+ b) bigInt
+ c) string ""
+ d) boolean  => true/false
+ e) null=> standalone value
+ f) undefined => value not assigned
+ g) symbol => unique
+
+ a) Object 
+ */
+
+console.table([typeof(userName),typeof(age),typeof(isLoggedIn),typeof(temp),typeof(userState)])
+
+ 
