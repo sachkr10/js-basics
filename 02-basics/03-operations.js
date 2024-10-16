eec6181ffb3b79f2bc52daccb9715955d4b79ce2
sachkr10 <64580037+sachkr10@users.noreply.github.com>
2024-10-16 16:16:54 +0000

    Operations in Java Script

---
+++ 02-basics/03-operations.js
@@ -0,0 +1,40 @@
+let value = 3;
+let negValue = -value
+console.log(negValue);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+// console.log(2 ** 3);
+
+let str1 = "Hello";
+let str2 = " Abc"
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 +3);
+// console.log(1 +"2" +3);
+// console.log(1 +2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter ++;
+console.log(gameCounter);
+
+/*
+for more details on the incremental operator please go through the below link 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment 
+*/
+
+
+
+
+
+
+
