70bd5eda9a6a92171e6161d41a3f0a202f297097
sachkr10 <64580037+sachkr10@users.noreply.github.com>
2024-10-16 16:35:53 +0000

    Comparision basics in Java Script

---
+++ 02-basics/04-comparisionsofdatatypes.js
@@ -0,0 +1,19 @@
+console.log(3>4);
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+
+/* line 9 as we are using greater than sign that will conver a nulll to a number
+  and we are getting result as true i.e., 0>=0
+  and comparision with undefined is alsways false
+*/
+
+console.log("Strict Check i.e, ===");
+
+console.log("2" === 2)  // witll return false as value and data types are being checked here 
+
